55794d827415ec4666b55cb95135c3ab57df6e6e
Lucky9-original <lucianito.araujo@gmail.com>
2019-08-13 18:50:34 +0100

    Primeira parte do projeto

---
+++ src/contratostrabalhador/ContratosTrabalhador.java
@@ -0,0 +1,14 @@
+package contratostrabalhador;
+
+import java.util.Scanner;
+
+public class ContratosTrabalhador {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner tec = new Scanner(System.in);
+        
+        
+    }
+    
+}
